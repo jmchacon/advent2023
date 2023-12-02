df3929fcb6892e94497af286af2287ac2db829ce
James Chacon <chacon.james@gmail.com>
2023-12-01 22:15:31 -0800

    day2 part 1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,15 +1,10 @@
 //! day2 advent 20XX
 use clap::Parser;
 use color_eyre::eyre::Result;
-use grid::{Grid, Location};
-use itertools::Itertools;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
-use strum_macros::Display;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -19,6 +14,15 @@ struct Args {
 
     #[arg(long, default_value_t = false)]
     debug: bool,
+
+    #[arg(long, default_value_t = 12)]
+    red: usize,
+
+    #[arg(long, default_value_t = 13)]
+    green: usize,
+
+    #[arg(long, default_value_t = 14)]
+    blue: usize,
 }
 
 fn main() -> Result<()> {
@@ -29,6 +33,50 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut total = 0;
+    for (line_num, line) in lines.iter().enumerate() {
+        let parts = line.split_whitespace().collect::<Vec<_>>();
+        assert!(parts[0] == "Game", "Invalid line {}: {line}", line_num + 1);
+        let game = parts[1].trim_end_matches(':').parse::<usize>().unwrap();
+        assert!(parts.len() % 2 == 0, "Uneven line {}: {line}", line_num + 1);
+        let rem = parts[2..].join(" ");
+        let balls = rem.split(';').collect::<Vec<_>>();
+
+        let mut good = true;
+        for b in &balls {
+            let parts = b.split_whitespace().collect::<Vec<_>>();
+            for i in (0..parts.len()).step_by(2) {
+                let num = parts[i].parse::<usize>().unwrap();
+                match parts[i + 1].trim_end_matches(',') {
+                    "red" => {
+                        if num > args.red {
+                            good = false;
+                        }
+                    }
+                    "blue" => {
+                        if num > args.blue {
+                            good = false;
+                        }
+                    }
+                    "green" => {
+                        if num > args.green {
+                            good = false;
+                        }
+                    }
+                    _ => panic!(),
+                }
+                if !good {
+                    break;
+                }
+            }
+            if !good {
+                break;
+            }
+        }
+        if good {
+            total += game;
+        }
+    }
+    println!("part1 - {total}");
     Ok(())
 }
