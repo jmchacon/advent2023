768525b0967fb8f583473fa0ec6d5a880d4d5a95
James Chacon <chacon.james@gmail.com>
2023-12-02 15:08:16 -0800

    Cleanup day1 and document it.
    Found a way with zip to avoid conditionals later and just compute part2 directly.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
+use std::iter::zip;
 use std::path::Path;
 
 #[derive(Parser)]
@@ -25,12 +26,32 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
+    // Input looks like:
+    //
+    // 4nineeightseven2
+    // 1abc2
+    //
+    // For part1 just find the left most digit and right most digit and make
+    // a 2 digit number out of them. For a single digit case like:
+    //
+    // two1nine
+    //
+    // It would compute to 11 then. Sum up all the numbers.
+    //
+    // For part2 spelled out digits are now possible and again we want the
+    // left most and right most ones. So the example above instead of 11 actually
+    // becomes 29. Again sum these up.
+
     let mut sum = 0;
     let mut sum2 = 0;
+    // Make 2 different matches arrays since we have to do digits by themselves
+    // before combining with alpha style.
     let matches = [
-        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
-    let digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    // A quick map of the string -> numeric so we don't have to add
+    // X.parse::<usize>().unwrap() everywhere.
     let to_digits = HashMap::from([
         ("1", 1),
         ("2", 2),
@@ -51,14 +72,44 @@ fn main() -> Result<()> {
         ("eight", 8),
         ("nine", 9),
     ]);
+
     for line in &lines {
+        // Skip empty lines in case the file got extra somehow.
         if line.is_empty() {
             continue;
         }
-        let digits_m = digits[1..]
+
+        // Run over the digits and map to all the places each one matches in the string.
+        let digits_m = digits
             .iter()
             .map(|f| line.match_indices(f).collect::<Vec<_>>())
             .collect::<Vec<_>>();
+        // Do the same with the alpha matches.
+        let full_m = matches
+            .iter()
+            .map(|f| line.match_indices(f).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+
+        // Compute the alpha min by zipping together the digits matches and the alpha matches.
+        // Then as we will get each sub vector as pairs merge those together and take the min
+        // of that but use filter_map to eliminate all the None's and unwrap implicitly. At this
+        // point index doesn't matter since the match_indicies filled in the string value so we
+        // can use the map to look that up later.
+        // After we have the reduced set take another min() on that to get the final value and then
+        // unwrap it. This can't be None if the string is proper (has at least 1 number or 1 alpha case).
+        // If not unwrap panic is fine here.
+        let full_min = zip(digits_m.clone(), full_m.clone())
+            .filter_map(|f| itertools::merge(f.0, f.1).min())
+            .min()
+            .unwrap();
+        // Do the same as above but get the max. Due to the way min/max work can't avoid 2 iterations like this.
+        let full_max = zip(digits_m.clone(), full_m.clone())
+            .filter_map(|f| itertools::merge(f.0, f.1).max())
+            .max()
+            .unwrap();
+
+        // Digits are generally the same except we need unwrap_or() instead since we may have a string
+        // of only alpha. In that case a sentinel just means the part1 case below gets skipped.
         let digits_min = digits_m
             .iter()
             .filter_map(|f| f.iter().min())
@@ -69,36 +120,12 @@ fn main() -> Result<()> {
             .filter_map(|f| f.iter().max())
             .max()
             .unwrap_or(&(usize::MIN, ""));
-        let matches_m = matches[1..]
-            .iter()
-            .map(|f| line.match_indices(f).collect::<Vec<_>>())
-            .collect::<Vec<_>>();
-        let matches_min = matches_m
-            .iter()
-            .filter_map(|f| f.iter().min())
-            .min()
-            .unwrap_or(&(usize::MAX, ""));
-        let matches_max = matches_m
-            .iter()
-            .filter_map(|f| f.iter().max())
-            .max()
-            .unwrap_or(&(usize::MIN, ""));
 
         // If we didn't find any digits just skip sum since it only involved them.
-        // If we didn't find alpha matches that's ok since the unwrap_or above
-        // will handle that with sentinel values.
         if digits_min.0 != usize::MAX {
             sum += to_digits[digits_min.1] * 10 + to_digits[digits_max.1];
         }
-        sum2 += if digits_min.0 < matches_min.0 {
-            to_digits[digits_min.1] * 10
-        } else {
-            to_digits[matches_min.1] * 10
-        } + if digits_max.0 >= matches_max.0 {
-            to_digits[digits_max.1]
-        } else {
-            to_digits[matches_max.1]
-        };
+        sum2 += to_digits[full_min.1] * 10 + to_digits[full_max.1];
     }
     println!("part1 - {sum}");
     println!("part2 - {sum2}");
