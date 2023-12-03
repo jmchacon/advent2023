b643dd3f3c28b67e6bec5e22e68ca2c8389cbab5
James Chacon <chacon.james@gmail.com>
2023-12-02 21:47:56 -0800

    day3 part1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,9 +2,6 @@
 use clap::Parser;
 use color_eyre::eyre::Result;
 use grid::{Grid, Location};
-use itertools::Itertools;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -21,6 +18,14 @@ struct Args {
     debug: bool,
 }
 
+#[derive(Clone, Debug, Default, Display, PartialEq, Eq)]
+enum Space {
+    Digit(usize),
+    Symbol,
+    #[default]
+    None,
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
     let args: Args = Args::parse();
@@ -29,6 +34,55 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut grid = Grid::<Space>::new(lines[0].len(), lines.len());
+    for (line_num, line) in lines.iter().enumerate() {
+        for (pos, b) in line.as_bytes().iter().enumerate() {
+            let l = Location(pos.try_into().unwrap(), line_num.try_into().unwrap());
+            if *b == b'.' {
+                continue;
+            }
+            if b.is_ascii_digit() {
+                grid.add(&l, Space::Digit(line[pos..=pos].parse::<usize>().unwrap()));
+                continue;
+            }
+            grid.add(&l, Space::Symbol);
+        }
+    }
+
+    let mut num_start = false;
+    let mut num_digits = vec![];
+    let mut has_symbol = false;
+    let mut sum = 0;
+    for g in &grid {
+        if *g.1 == Space::None || *g.1 == Space::Symbol {
+            if num_start && has_symbol {
+                sum += compute_num(&num_digits);
+            }
+            num_start = false;
+            continue;
+        }
+        if let Space::Digit(d) = g.1 {
+            if !num_start {
+                has_symbol = false;
+                num_digits.clear();
+            }
+            num_start = true;
+            num_digits.push(*d);
+            if !has_symbol {
+                has_symbol = grid
+                    .neighbors_all(&g.0)
+                    .iter()
+                    .any(|f| *f.1 == Space::Symbol);
+            }
+        }
+    }
+    println!("part1: {sum}");
     Ok(())
+}
+
+fn compute_num(num_digits: &Vec<usize>) -> usize {
+    let l = num_digits.len() - 1;
+    num_digits.iter().enumerate().fold(0, |acc, f| {
+        acc + *f.1 * 10_usize.pow((l - f.0).try_into().unwrap())
+    })
 }
