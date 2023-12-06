2e1105826f200bd7cc2778e41842a618e794c42d
James Chacon <chacon.james@gmail.com>
2023-12-05 21:55:54 -0800

    day5 part1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,15 +1,11 @@
 //! day5 advent 20XX
 use clap::Parser;
 use color_eyre::eyre::Result;
-use grid::{Grid, Location};
-use itertools::Itertools;
-use slab_tree::tree::TreeBuilder;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
-use strum_macros::Display;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -29,6 +25,80 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut maps = HashMap::new();
+    let mut map = vec![];
+    let mut src = "";
+    let mut dest = "";
+
+    // Just parse seeds direct and then start parsing.
+    assert!(
+        lines[0].starts_with("seeds:"),
+        "1st line invalid. Need 'seeds: '"
+    );
+    assert!(lines[1].is_empty(), "line 2 isn't blank");
+    let seeds = lines[0]
+        .split_whitespace()
+        .skip(1)
+        .map(|f| f.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    for (line_num, line) in lines[2..].iter().enumerate() {
+        if line.is_empty() {
+            maps.insert(src, (dest, map));
+            map = vec![];
+            src = "";
+            continue;
+        }
+
+        let parts = line.split_whitespace().collect::<Vec<_>>();
+        if src.is_empty() {
+            assert!(parts[1] == "map:", "Not a proper map line {}", line_num + 3);
+            let sub = parts[0].split('-').collect::<Vec<_>>();
+            src = sub[0];
+            dest = sub[2];
+            continue;
+        }
+
+        // Anything is parsing the current map
+        assert!(parts.len() == 3, "Invalid data line {}", line_num + 3);
+        map.push(
+            parts
+                .iter()
+                .map(|f| f.parse::<usize>().unwrap())
+                .collect::<Vec<_>>(),
+        );
+    }
+    if !src.is_empty() {
+        maps.insert(src, (dest, map));
+    }
+
+    let mut min = usize::MAX;
+    let mut cur = "seed";
+    let mut id;
+    for s in &seeds {
+        id = *s;
+        loop {
+            let m = &maps[cur];
+
+            for r in &m.1 {
+                let dest = r[0];
+                let src = r[1];
+                let rng = r[2];
+                if id >= src && id < src + rng {
+                    id = id - src + dest;
+                    break;
+                }
+            }
+            cur = m.0;
+            if !maps.contains_key(cur) {
+                if id < min {
+                    min = id;
+                }
+                cur = "seed";
+                break;
+            }
+        }
+    }
+    println!("part1 - {min}");
     Ok(())
 }
