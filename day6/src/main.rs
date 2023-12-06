e8d5aaa36adbe66788402223fb29c9e520561e5b
James Chacon <chacon.james@gmail.com>
2023-12-05 22:38:15 -0800

    day6 parts1 and 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,15 +1,10 @@
 //! day6 advent 20XX
 use clap::Parser;
 use color_eyre::eyre::Result;
-use grid::{Grid, Location};
-use itertools::Itertools;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
-use strum_macros::Display;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -29,6 +24,54 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    assert!(lines[0].starts_with("Time:"), "Invalid first line");
+    assert!(lines[1].starts_with("Distance:"), "Invalid second line");
+
+    let times = lines[0]
+        .split_whitespace()
+        .skip(1)
+        .map(|f| f.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let distances = lines[1]
+        .split_whitespace()
+        .skip(1)
+        .map(|f| f.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    println!("{times:?}");
+    println!("{distances:?}");
+
+    let mut ways = vec![];
+    for (pos, t) in times.iter().enumerate() {
+        let mut way = 0;
+        // Skip first and last as their distance is always 0
+        for i in 1..*t {
+            let dist = (*t - i) * i;
+            if dist > distances[pos] {
+                way += 1;
+            }
+        }
+        ways.push(way);
+    }
+    println!("part1 - {}", ways.iter().product::<usize>());
+    let time = lines[0].split_whitespace().collect::<Vec<_>>()[1..]
+        .join("")
+        .parse::<usize>()
+        .unwrap();
+    let distance = lines[1].split_whitespace().collect::<Vec<_>>()[1..]
+        .join("")
+        .parse::<usize>()
+        .unwrap();
+    println!("{time}");
+    println!("{distance}");
+    let mut way = 0;
+    // Skip first and last as their distance is always 0
+    for i in 1..time {
+        let dist = (time - i) * i;
+        if dist > distance {
+            way += 1;
+        }
+    }
+    println!("part2 - {way}");
+
     Ok(())
 }
