fe23f5070451cea481a20c84feae0bad840dfb7c
James Chacon <chacon.james@gmail.com>
2023-12-07 22:15:08 -0800

    day8 part1 and part2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,15 +1,11 @@
 //! day8 advent 20XX
 use clap::Parser;
 use color_eyre::eyre::Result;
-use grid::{Grid, Location};
-use itertools::Itertools;
-use slab_tree::tree::TreeBuilder;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
-use strum_macros::Display;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -29,6 +25,57 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let steps = lines[0].as_str();
+    let mut conditions = HashMap::new();
+    for line in &lines[2..] {
+        let parts = line.split_whitespace().collect::<Vec<_>>();
+        let left = parts[2].trim_end_matches(',').trim_start_matches('(');
+        let right = parts[3].trim_end_matches(')');
+        conditions.insert(parts[0], (left, right));
+    }
+
+    if conditions.contains_key("AAA") {
+        let tot = find_total("AAA", &conditions, steps);
+        println!("part1: {tot}");
+    }
+
+    // For part2 we need to find everything ending with "A" as a start position
+    // and compute their path. Then the way to get them all together is to get the
+    // LCM of all 6 numbers which is the fold at the end.
+    let curs = conditions
+        .keys()
+        .filter(|f| f.ends_with('A'))
+        .copied()
+        .map(|f| find_total(f, &conditions, steps))
+        .fold(1, num::integer::lcm);
+    println!("part2: {curs}");
     Ok(())
+}
+
+fn find_total(start: &str, conditions: &HashMap<&str, (&str, &str)>, steps: &str) -> usize {
+    let mut tot = 0;
+    let mut cur = start;
+    let mut done = false;
+    loop {
+        for s in steps.as_bytes() {
+            match s {
+                b'R' => {
+                    cur = conditions[cur].1;
+                }
+                b'L' => {
+                    cur = conditions[cur].0;
+                }
+                _ => panic!(),
+            }
+            tot += 1;
+            if cur.ends_with('Z') {
+                done = true;
+                break;
+            }
+        }
+        if done {
+            break;
+        }
+    }
+    tot
 }
