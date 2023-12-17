bb34d66c94aac8a3d763cae61678a44f14295ff1
James Chacon <chacon.james@gmail.com>
2023-12-16 22:31:19 -0800

    day9 part2
    Reasonable simple. Do the inverse to find the new left most value on the first row and sum those up.
    
    Just different math working backwards.

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -25,6 +25,7 @@ fn main() -> Result<()> {
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
     let mut sum = 0;
+    let mut sum_part2 = 0;
     for line in &lines {
         // Seed the initial line.
         let mut parts = line
@@ -34,6 +35,7 @@ fn main() -> Result<()> {
 
         // Keep a record of the last entry for each line.
         let mut diffs = vec![*parts.last().unwrap()];
+        let mut fronts = vec![*parts.first().unwrap()];
         loop {
             if args.debug {
                 println!("parts: {parts:?}");
@@ -43,6 +45,7 @@ fn main() -> Result<()> {
                 next.push(parts[i + 1] - parts[i]);
             }
             diffs.push(*next.last().unwrap());
+            fronts.push(*next.first().unwrap());
             parts = next;
             if parts.iter().any(|f| *f != 0) {
                 continue;
@@ -53,7 +56,15 @@ fn main() -> Result<()> {
             println!("diffs: {diffs:?}");
         }
         sum += diffs.iter().sum::<isize>();
+        fronts.reverse();
+        let mut last = 0;
+        for i in fronts.iter().skip(1) {
+            let new = *i - last;
+            last = new;
+        }
+        sum_part2 += last;
     }
     println!("part1: {sum}");
+    println!("part2: {sum_part2}");
     Ok(())
 }
