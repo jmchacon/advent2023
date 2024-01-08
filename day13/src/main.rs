deeb6716089ef87bd00f0296ed79e114f5a16adb
James Chacon <chacon.james@gmail.com>
2024-01-07 22:08:23 -0800

    Day13 part2
    Refactor into a bunch of functions so this can be called multiple times with different variations.

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -47,42 +47,76 @@ fn main() -> Result<()> {
     }
     patterns.push(pattern);
 
+    // Run through the patterns and find the one without any changes.
+    // Then do it again requiring a smudge fix.
     let mut sum = 0;
+    let mut sum_part2 = 0;
     for p in &patterns {
-        if args.debug {
-            print_grid(p);
-        }
-        if let Some(r) = find_mirror(p, args.debug) {
-            sum += 100 * r;
-            if args.debug {
-                println!("Found mirror and {r} rows above.");
-            }
-        } else {
-            let c = column_vec(p);
-            if args.debug {
-                print_grid(&c);
-            }
-            let r = find_mirror(&c, args.debug).unwrap();
-            sum += r;
-            if args.debug {
-                println!("Found mirror and {r} rows to the left.");
-            }
-        }
+        find_reflect(p, args.debug, false, &mut sum);
+        find_reflect(p, args.debug, true, &mut sum_part2);
     }
     println!("part1: {sum}");
+    println!("part2: {sum_part2}");
     Ok(())
 }
 
-fn find_mirror(p: &[Vec<i32>], debug: bool) -> Option<usize> {
+// For a given grid try and find a mirror horizontally first, then flip it
+// into a column rotation and try again. The input says we're guarenteed to find
+// a solution in this.
+fn find_reflect(p: &[Vec<i32>], debug: bool, find_smudge: bool, sum: &mut usize) {
+    if debug {
+        print_grid(p);
+    }
+    if let Some(r) = find_mirror(p, debug, find_smudge) {
+        *sum += 100 * r;
+        if debug {
+            println!("Found mirror and {r} rows above.");
+        }
+    } else {
+        let c = column_vec(p);
+        if debug {
+            print_grid(&c);
+        }
+        // Input claims we're guarenteed to find a reflection so if the
+        // above didn't this must or something is broken..
+        let r = find_mirror(&c, debug, find_smudge).unwrap();
+        *sum += r;
+        if debug {
+            println!("Found mirror and {r} rows to the left.");
+        }
+    }
+}
+
+// For each row in a given grid walk from the 2nd row onwards comparing
+// (via rows_equal so we can account for smudge correction). Each time
+// we find a match loop expanding the rows out to match until we hit an edge.
+// If we matched all the way back to the edge this is a valid mirror. Otherwise
+// one didn't exist.
+fn find_mirror(p: &[Vec<i32>], debug: bool, find_smudge: bool) -> Option<usize> {
     for pp in 1..p.len() {
         #[allow(clippy::cast_possible_wrap)]
         let mut right = pp as isize;
         let mut gap = 1_isize;
+        let mut ret_find_smudge = find_smudge;
         loop {
             #[allow(clippy::cast_sign_loss)]
-            if p[right as usize] == p[(right - gap) as usize] {
+            let ret = rows_equal(
+                &p[right as usize],
+                &p[(right - gap) as usize],
+                debug,
+                ret_find_smudge,
+            );
+            if ret.0 {
                 if debug {
-                    println!("Found at {right} and {}", right - gap);
+                    println!(
+                        "Found at {right} and {} with smudge: {find_smudge}",
+                        right - gap
+                    );
+                }
+                if ret.1 {
+                    // Once we've corrected one place and gotten a further match we quit correcting for
+                    // this iteration.
+                    ret_find_smudge = false;
                 }
                 if right + 1 < p.len().try_into().unwrap() && ((right + 1) - (gap + 2)) >= 0 {
                     right += 1;
@@ -92,6 +126,16 @@ fn find_mirror(p: &[Vec<i32>], debug: bool) -> Option<usize> {
                     }
                     continue;
                 }
+
+                // If we must find a smudge and we've matched completely but
+                // never corrected (i.e. ret_find_smudge is still true) then
+                // this isn't a match for part2.
+                if find_smudge {
+                    if ret_find_smudge {
+                        break;
+                    }
+                    return Some(pp);
+                }
                 return Some(pp);
             }
             break;
@@ -100,6 +144,42 @@ fn find_mirror(p: &[Vec<i32>], debug: bool) -> Option<usize> {
     None
 }
 
+// For 2 rows check if they're equal. If not and we require finding a smudge
+// correction start trying to change each entry for one row until this either
+// does match or we can't find a match for this pair of rows.
+//
+// Returns (found, corrected) where found indicates if the rows matched and
+// corrected indicates a smudge correction was used to perform this.
+//
+// NOTE: In real code this would be a struct as bool, bool is confusing to keep straight otherwise.
+fn rows_equal(p1: &Vec<i32>, p2: &Vec<i32>, debug: bool, find_smudge: bool) -> (bool, bool) {
+    if *p1 == *p2 {
+        return (true, false);
+    }
+    if !find_smudge {
+        return (false, false);
+    }
+
+    let mut smudge_p2 = p2.clone();
+    for i in 0..smudge_p2.len() {
+        let orig = smudge_p2[i];
+        if orig == 0 {
+            smudge_p2[i] = 1;
+        } else {
+            smudge_p2[i] = 0;
+        }
+        if smudge_p2 == *p1 {
+            if debug {
+                println!("Found smudge at position {i}");
+            }
+            return (true, true);
+        }
+        smudge_p2[i] = orig;
+    }
+    (false, false)
+}
+
+// Take a grid and rotate it clockwise 90 degrees and return a new grid.
 fn column_vec(pattern: &[Vec<i32>]) -> Vec<Vec<i32>> {
     // Pre-reserve capacity in each so we can just iterate and place.
     let mut ret = vec![vec![0; pattern.len()]; pattern[0].len()];
@@ -114,7 +194,8 @@ fn column_vec(pattern: &[Vec<i32>]) -> Vec<Vec<i32>> {
     ret
 }
 
-fn print_grid(pattern: &Vec<Vec<i32>>) {
+// Print out the given grid for debugging.
+fn print_grid(pattern: &[Vec<i32>]) {
     for y in pattern {
         for x in y {
             if *x == 0 {
